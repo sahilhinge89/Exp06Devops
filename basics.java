4c69095c6ca8244b4c3fb0d2a84d89c3b50f3d30
sahil <sahilhinge89@gmail.com>
2024-09-10 23:22:37 +0530

    initial commit

---
+++ basics.java
@@ -0,0 +1,6 @@
+public class basics {
+    public static void main(String[] args) {
+        // This prints "Hello, World!" to the console
+        System.out.println("Lord Pravin Patil");
+    }
+}
